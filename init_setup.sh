34a76db1d76a4a44352d55cea92a728cdc446db0
ishawant kumar <ishawantkumar@gmail.com>
2023-01-21 12:20:18 +0530

    updated init_setup.py

--- init_setup.sh
+++ init_setup.sh
@@ -6,3 +6,6 @@ source activate ./env
 echo [$(date)]: "installing the dev requirements"
 pip install -r requirements_dev.txt 
 echo [$(date)]: "END"
+
+
+# for run this file write command bash init_setup.py 
